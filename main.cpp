4474746bd80842651f1904009deb2a319970eee2
Mario <mario.bambagini@gmail.com>
2020-04-20 21:45:54 +0100

    first draft

--- main.cpp
+++ main.cpp
@@ -1,64 +1,96 @@
 #include "mbed.h"
 
-Serial pc(USBTX, USBRX);
+#include "xbee.hpp"
+#include "ssWiSocket.hpp"
 
-DigitalOut pc_activity(LED1);
+#define SENSOR_PORT_ID          1
+#define MOTOR_RIGHT_PORT_ID     3
+#define MOTOR_LEFT_PORT_ID      5
 
-int speed_left;
-int speed_right;
+#define TX_SPEED_UPLOAD_MS      500
+#define FRAME_RATE_MS           800
+#define MAIL_LOOP_MS            50
 
-void print_status() {
-    printf("\r\nSystem status:\r\n");
-    printf("Speed: %d%% %d%%\r\n", speed_left, speed_right);
-    printf("Press 'h' for help\r\n");
-}
+#define SPEED_FACTOR            1
+#define MAX_SPEED               100
+#define MIN_SPEED               -100
 
-#define SPEED_FACTOR  5
-#define MAX_SPEED 100
-#define MIN_SPEED -100
+void print_status();
+void process_input(unsigned char c);
 
-bool process_input(unsigned char c) {
-    switch(c) {
-        case 'q':
-            if (speed_left < MAX_SPEED)
-                speed_left = speed_left + SPEED_FACTOR;
-            break;
-        case 'a':
-            if (speed_left > MIN_SPEED)
-                speed_left = speed_left - SPEED_FACTOR;
-            break;
-        case 'w':
-            if (speed_right < MAX_SPEED)
-                speed_right = speed_right + SPEED_FACTOR;
-            break;
-        case 's':
-            if (speed_right > MIN_SPEED)
-                speed_right = speed_right - SPEED_FACTOR;
-            break;
-        case 'h':
-            printf("'q/w': increment left/right motor\n\r");
-            printf("'a/s': decrement left/right motor\n\r");
-            break;
-        default:
-            return false;
-    }
-    return true;
-}
+int speed_left = 0;
+int speed_right = 0;
 
-void init() {
-    speed_left = 0;
-    speed_right = 0;
-}
+Serial pc(USBTX, USBRX, "pc", 9600);
 
 int main() {
+    int counterTx = 0;
+    int counterUpdate = 0;
+    constexpr int loopsBeforeUpload = int(TX_SPEED_UPLOAD_MS/MAIL_LOOP_MS);
+    constexpr int loopsBeforeFrameUpdate = int(FRAME_RATE_MS/MAIL_LOOP_MS);
+
+    // init ssWi protocol
+    XBeeModule xbee(p9, p10, 102, 14);
+    xbee.setDstAddress(XBeeBroadcastAddress());
+    xbee.init(5, 10);
+    
+    ssWiSocket* leftSocket = ssWiSocket::createSocket(MOTOR_LEFT_PORT_ID);
+    ssWiSocket* rightSocket = ssWiSocket::createSocket(MOTOR_RIGHT_PORT_ID);
+
     print_status();
-    while(1) {
-        if(pc.readable()) {
+
+    while(true) {
+        // process user input
+        if (pc.readable()) {
             unsigned char c = pc.getc();
-            if (process_input(c)) {
-                pc_activity = !pc_activity;
-                print_status();
-            }
+            process_input(c);
         }
+        // tx speeds
+        if (++counterTx == loopsBeforeUpload) {
+            counterTx = 0;
+            leftSocket->write(speed_left);
+            rightSocket->write(speed_right);
+        }
+        // update frame
+        if (++counterUpdate == loopsBeforeFrameUpdate) {
+            counterUpdate = 0;
+            print_status();
+        }
+
+        // main loop end
+        thread_sleep_for(MAIL_LOOP_MS);
+    }
+}
+
+void print_status() {
+    static ssWiSocket* sensorSocket = ssWiSocket::createSocket(SENSOR_PORT_ID);
+    printf("Distance (%dcm) - Speed (%d%% %d%%)\r\n", sensorSocket->read(), 
+                                                      speed_left, speed_right);
+}
+
+void print_help() {
+    printf("*********************************\n\r");
+    printf("'q/w': increment left/right motor\n\r");
+    printf("'a/s': decrement left/right motor\n\r");
+    printf("*********************************\n\r");
+}
+
+void process_input(unsigned char c) {
+    switch(c) {
+        case 'q':
+            speed_left += (speed_left < MAX_SPEED) ? SPEED_FACTOR : 0;
+            break;
+        case 'a':
+            speed_left -= (speed_left > MIN_SPEED) ? SPEED_FACTOR : 0;
+            break;
+        case 'w':
+            speed_right += (speed_right < MAX_SPEED) ? SPEED_FACTOR : 0;
+           break;
+        case 's':
+            speed_right -= (speed_right > MIN_SPEED) ? SPEED_FACTOR : 0;
+            break;
+        case 'h':
+            print_help();
+            break;
     }
 }
